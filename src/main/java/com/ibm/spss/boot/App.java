b3e937a7b6ef8c804708a5dd59e4a2eb2a62c380
jncz <li.j2ee@gmail.com>
2015-03-21 22:25:37 +0800

    remove unused import

--- src/main/java/com/ibm/spss/boot/App.java
+++ src/main/java/com/ibm/spss/boot/App.java
@@ -3,8 +3,6 @@ package com.ibm.spss.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
 @ComponentScan
