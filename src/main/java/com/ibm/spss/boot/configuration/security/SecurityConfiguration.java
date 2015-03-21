f724c117b324be76b036740b21fcdb4b55e83389
jncz <li.j2ee@gmail.com>
2015-03-21 23:01:02 +0800

    add switch to turn on/off the security feature, the default is off

--- src/main/java/com/ibm/spss/boot/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/ibm/spss/boot/configuration/security/SecurityConfiguration.java
@@ -1,13 +1,17 @@
 package com.ibm.spss.boot.configuration.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.ibm.spss.boot.util.RunningProfile;
 
 @EnableAutoConfiguration
 @ComponentScan
@@ -15,6 +19,7 @@ public class SecurityConfiguration {
 	
     @Configuration
 	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    @Profile(RunningProfile.Security)
 	protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
 		@Autowired
@@ -36,5 +41,27 @@ public class SecurityConfiguration {
 		}
 
 	}
+    
+    @Configuration
+   	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    @ConditionalOnMissingBean(ApplicationSecurity.class)
+   	protected static class EmptyApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+   		@Autowired
+   		private SecurityProperties security;
+
+   		@Override
+   		protected void configure(HttpSecurity http) throws Exception {
+   			http.authorizeRequests().anyRequest().permitAll();
+   		}
+
+   		@Override
+   		public void configure(AuthenticationManagerBuilder auth) throws Exception {
+   			auth.inMemoryAuthentication().withUser("admin").password("admin")
+   					.roles("ADMIN", "USER").and().withUser("user").password("user")
+   					.roles("USER");
+   		}
+
+   	}
 
 }
--- src/main/java/com/ibm/spss/boot/util/RunningProfile.java
+++ src/main/java/com/ibm/spss/boot/util/RunningProfile.java
@@ -3,6 +3,7 @@ package com.ibm.spss.boot.util;
 public interface RunningProfile {
 	String Dev = "dev";
 	String Production = "production";
+	String Security = "security";
 	
 	String SimpleCache = "simple_cache";
 	String ExternalCache = "cache";
